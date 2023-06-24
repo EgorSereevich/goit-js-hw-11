2a0c82976e46c519c89097b9a81a9775a3a86882
Egor Sergeevich <122815068+EgorSereevich@users.noreply.github.com>
2023-06-24 12:37:28 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import Notiflix from 'notiflix';
+// import SimpleLightbox from 'simplelightbox';
+const HREF_PIX = 'https://pixabay.com/api/';
+const KEY_PIX = '37771567-c63b0fa1e82728e8a21c21132';
+const refs = {
+  formEl: document.querySelector('.search-form'),
+  inputEl: document.querySelector('input'),
+  buttonEl: document.querySelector('button'),
+  cardEl: document.querySelector('.gallery'),
+};
